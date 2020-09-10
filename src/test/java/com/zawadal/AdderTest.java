22fbe0892cf64cdf5e227c5064161db0fcc326e6
Lukasz184396 <zawadalukasz88@gmail.com>
2020-09-10 13:09:43 +0200

    one thread adder

---
+++ src/test/java/com/zawadal/AdderTest.java
@@ -0,0 +1,35 @@
+package com.zawadal;
+
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AdderTest {
+
+    public static String BASE_DIR = "src/test/resources/";
+    //single thread
+    @Test
+    public void doAddTest() {
+        String[] inFiles = {BASE_DIR + "files1.txt", BASE_DIR + "files2.txt", BASE_DIR + "files3.txt", //
+                BASE_DIR + "files4.txt", BASE_DIR + "files5.txt", BASE_DIR + "files6.txt" };
+
+        String[] outFiles = {BASE_DIR + "filesout1.txt", BASE_DIR + "filesout2.txt", BASE_DIR + "filesout3.txt",  //
+         BASE_DIR + "filesout4.txt", BASE_DIR + "filesout5.txt", BASE_DIR + "filesout6.txt"};
+//        String[] outFiles = {"./filesout1.txt", "./filesout2.txt", "./filesout3.txt", //
+//                "./filesout4.txt", "./filesout5.txt", "./filesout6.txt"};
+
+        try {
+            for (int i = 0; i < inFiles.length; i++) {
+                Adder adder = new Adder(inFiles[i], outFiles[i]);
+                adder.doAdd();
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+
+        }
+
+    }
+
+}
